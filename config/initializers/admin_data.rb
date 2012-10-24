52e2c15d37b48f80063e9d2bcad3f5596b75e3a6
bhennes2 <bhennes2@gmail.com>
2012-10-24 08:39:28 -0500

    Fix syntax error on admin_data intializer

--- config/initializers/admin_data.rb
+++ config/initializers/admin_data.rb
@@ -1,4 +1,4 @@
 AdminData.config do |config|
-  config.is_allowed_to_view = lambda {|controller| if Rails.env.development? || Rails.env.production? }
-  config.is_allowed_to_update = lambda {|controller| if Rails.env.development? || Rails.env.production? }
+  config.is_allowed_to_view = lambda { |controller| Rails.env.development? || Rails.env.production? }
+  config.is_allowed_to_update = lambda { |controller| Rails.env.development? || Rails.env.production? }
 end
