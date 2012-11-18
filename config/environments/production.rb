851770f0170efebaf5a2df157149698ce219ab31
bhennes2 <bhennes2@gmail.com>
2012-11-18 16:19:09 -0600

    Remove exception_notification from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,9 @@
 EventBase::Application.configure do
   
-  config.middleware.use ExceptionNotifier,
-    :email_prefix => "Error in EventBase",
-    :sender_address => %{"notifier" <notifier@example.com>},
-    :exception_recipients => %w{'bhennes2@gmail.com' 'launchcafe@gmail.com'}
+  # config.middleware.use ExceptionNotifier,
+  #   :email_prefix => "Error in EventBase",
+  #   :sender_address => %{"notifier" <notifier@example.com>},
+  #   :exception_recipients => %w{'bhennes2@gmail.com' 'launchcafe@gmail.com'}
   
   # Settings specified here will take precedence over those in config/application.rb
 
