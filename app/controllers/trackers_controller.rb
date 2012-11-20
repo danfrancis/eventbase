c430e3b45d64feccb3d4dc3e834db7d13447c0ca
bhennes2 <bhennes2@gmail.com>
2012-11-20 17:42:23 -0600

    Event or company can be on multiple lists, create new list from dropdown

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -1,10 +1,24 @@
 class TrackersController < ApplicationController
   
   def create
-    params[:tracker] = { list_id: params[:list_id], user_id: current_user.id, tracking_type: params[:tracking_type], tracking_id: params[:tracking_id] }
-    tracker = Tracker.new(params[:tracker])
-    tracker.save!
+    #current_kase.update_attributes(params[:kase])
+
+    params[:tracker][:list_id].each do |list_id|
+      if list_id.present?
+        params[:tracker] = { list_id: list_id, user_id: current_user.id, tracking_type: params[:tracking_type], tracking_id: params[:tracking_id] }
+        tracker = Tracker.new(params[:tracker])
+        tracker.save!
+      end
+    end
+    
+    if params[:new_list].present?
+      list = List.create!(list_type: params[:tracking_type], name: params[:new_list], user_id: current_user.id)
+      params[:tracker] = { list_id: list.id, user_id: current_user.id, tracking_type: params[:tracking_type], tracking_id: params[:tracking_id] }
+      tracker = Tracker.new(params[:tracker])
+      tracker.save!
+    end
     redirect_to :back
+    
   end
   
   def destroy
