d11f33f5b1c8e50b03201075c752e8f4434757d0
bhennes2 <bhennes2@gmail.com>
2012-12-09 15:05:35 -0600

    Prevent duplicates when creating a new list

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -4,8 +4,13 @@ class TrackersController < ApplicationController
   
   def create
     #current_kase.update_attributes(params[:kase])
-
-    if params[:tracker].present?
+    
+    if params[:new_list].present?
+      list = List.create!(list_type: params[:tracking_type], name: params[:new_list], user_id: current_user.id)
+      params[:tracker] = { list_id: list.id, user_id: current_user.id, tracking_type: params[:tracking_type], tracking_id: params[:tracking_id] }
+      tracker = Tracker.new(params[:tracker])
+      tracker.save!
+    elsif params[:tracker].present?
       params[:tracker][:list_id].each do |list_id|
         if list_id.present?
           params[:tracker] = { list_id: list_id, user_id: current_user.id, tracking_type: params[:tracking_type], tracking_id: params[:tracking_id] }
@@ -13,13 +18,6 @@ class TrackersController < ApplicationController
           tracker.save!
         end
       end
-    end
-    
-    if params[:new_list].present?
-      list = List.create!(list_type: params[:tracking_type], name: params[:new_list], user_id: current_user.id)
-      params[:tracker] = { list_id: list.id, user_id: current_user.id, tracking_type: params[:tracking_type], tracking_id: params[:tracking_id] }
-      tracker = Tracker.new(params[:tracker])
-      tracker.save!
     end
     redirect_to :back
     
