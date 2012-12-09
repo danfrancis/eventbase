7eba234dab1815be3a9b5ed01b1805716df26974
bhennes2 <bhennes2@gmail.com>
2012-12-09 14:59:37 -0600

    Dependent destroy for lists on their trackers

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,7 @@ class List < ActiveRecord::Base
   attr_accessible :list_type, :name, :user_id
   
   #Associations
-  has_many :trackers
+  has_many :trackers, dependent: :destroy
   belongs_to :users
   has_many :filters, as: :filterable
   has_many :events, through: :trackers, source_type: 'Event', source: :tracking
